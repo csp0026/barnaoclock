317005549180462b55b29db037b9dd3a8ff5e504
Claudia <csp0026@alu.medac.es>
2026-03-16 13:41:59 +0100

    falta q los filtros funcionen en explorar

--- filter.js
+++ filter.js
@@ -4,55 +4,30 @@ const priceFilter = document.getElementById("filter-price");
 
 const events = document.querySelectorAll(".card");
 
-function filterEvents(){
+function filterEvents() {
 
-let day = dayFilter.value;
-let genre = genreFilter.value;
-let price = priceFilter.value;
+    const day = dayFilter.value;
+    const genre = genreFilter.value;
+    const price = priceFilter.value;
 
-events.forEach(event => {
+    events.forEach(event => {
 
-let eventDay = event.dataset.day;
-let eventGenre = event.dataset.genre;
-let eventPrice = event.dataset.price;
+        const eventDay = event.dataset.day;
+        const eventGenre = event.dataset.genre;
+        const eventPrice = event.dataset.price;
 
-let showDay =
-day === "all" ||
-day === "alldays" ||
-day === eventDay;
+        const showDay = day === "all" || day === "alldays" || day === eventDay;
+        const showGenre = genre === "all" || genre === "allgenre" || genre === eventGenre;
 
-let showGenre =
-genre === "all" ||
-genre === "allgenre" ||
-genre === eventGenre;
+        let showPrice =
+    price === "all" || price === "allprices" ||
+    (price === "free" && eventPrice === "free") ||
+    (price === "paid15" && (eventPrice === "free" || eventPrice === "paid15")) ||
+    (price === "paid30" && (eventPrice === "free" || eventPrice === "paid15" || eventPrice === "paid30"));
 
-let showPrice = false;
+        event.style.display = (showDay && showGenre && showPrice) ? "" : "none";
 
-if(price === "all" || price === "allprices"){
-showPrice = true;
-}
-
-else if(price === "free" && eventPrice === "free"){
-showPrice = true;
-}
-
-else if(price === "paid15" && (eventPrice === "free" || eventPrice === "paid15")){
-showPrice = true;
-}
-
-else if(price === "paid30" && (eventPrice === "free" || eventPrice === "paid15" || eventPrice === "paid30")){
-showPrice = true;
-}
-
-if(showDay && showGenre && showPrice){
-event.style.display = "block";
-}
-else{
-event.style.display = "none";
-}
-
-});
-
+    });
 }
 
 dayFilter.addEventListener("change", filterEvents);
